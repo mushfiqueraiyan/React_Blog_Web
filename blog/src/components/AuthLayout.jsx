45231b6a7414b9f7bc444b6f8999d9e703df57db
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2024-05-13 20:53:52 +0600

    25 No out of 34 RTE

--- blog/src/components/AuthLayout.jsx
+++ blog/src/components/AuthLayout.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React,  {useState, useEffect} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-function AuthLayout() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Protected({children,authentication = true}) {
+  
+  const navigate = useNavigate()
+  const [loader, setLoader] = useState(true)
+  const authStatus = useSelector(state => state.auth.status)
+
+  useEffect(() => {
+    
+    if (authentication && authStatus !== authentication ) {
+      navigate("/login")
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/login")
+    }
+
+    setLoader(false)
+    
+  }, [authStatus, navigate, authentication])
+  
+  
+  return loader ? <h1>Loading...</h1> : <>{children}</>
+    
+  
 }
 
-export default AuthLayout
+
