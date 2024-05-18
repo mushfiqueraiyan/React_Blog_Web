3349f4b8dd332591c50f2e519684570285155cac
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2024-05-18 23:06:53 +0600

    postform

--- blog/src/components/RTE.jsx
+++ blog/src/components/RTE.jsx
@@ -8,7 +8,7 @@ export default function RTE({name, control, label, defaultValue= ""}) {
 
   return (
     
-    <div className='w-full'>
+    <div className='w-full' >
 
         {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
 
